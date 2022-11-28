f9871bf0b6774c1d5cb325f017df6bc5b4f80bb9
SkyrilHD <skyrilhd.info@gmail.com>
2022-11-28 23:21:18 +0100

    Add main.swift

---
+++ HotApps/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  HotApps
+//
+//  Created by SkyrilHD on 28.11.22.
+//
+
+import AppKit.NSApplication
+
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+app.hide(nil)
+app.setActivationPolicy(.prohibited)
+
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
