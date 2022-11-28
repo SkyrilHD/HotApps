02bd679cc177552e6eb952d0d2d2034732ab994e
SkyrilHD <skyrilhd.info@gmail.com>
2022-11-28 03:10:29 +0100

    Initial Commit

---
+++ HotApps/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HotApps
+//
+//  Created by SkyrilHD on 28.11.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
