91359559ad54e66ec2ed5c295fc0b191dc6aa813
SkyrilHD <skyrilhd.info@gmail.com>
2022-11-29 12:15:54 +0100

    add full path to app location

--- HotApps/AppDelegate.swift
+++ HotApps/AppDelegate.swift
@@ -9,15 +9,15 @@ import Cocoa
 
 // TODO: Save to UserDefaults
 // Example apps
-let blApp = "CotEditor.app"
-let brApp = "Meta.app"
-let tlApp = "Spek.app"
-let trApp = "Hex Fiend.app"
+let blApp = "/Applications/CotEditor.app"
+let brApp = "/Applications/Meta.app"
+let tlApp = "/Applications/Spek.app"
+let trApp = "/Applications/Hex Fiend.app"
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     
-
+    var appToOpen: String = ""
     var corner: Bool = false
     var appPath: URL?
 
@@ -31,27 +31,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             // This will check if the pointer is moved to any corner
             // bottom-left corner
             if NSEvent.mouseLocation.x == 0 && NSEvent.mouseLocation.y < 1 {
-                self.appPath = NSURL(fileURLWithPath: "/Applications/"+blApp, isDirectory: true) as URL
+                self.appToOpen = blApp
                 self.corner = true
             }
             // bottom-right corner
             else if (NSEvent.mouseLocation.x).rounded() >= NSScreen.main!.frame.maxX && NSEvent.mouseLocation.y < 1 {
-                self.appPath = NSURL(fileURLWithPath: "/Applications/"+brApp, isDirectory: true) as URL
+                self.appToOpen = brApp
                 self.corner = true
             }
             // top-left corner
             else if NSEvent.mouseLocation.x == 0 && (NSEvent.mouseLocation.y).rounded() >= NSScreen.main!.frame.maxY {
-                self.appPath = NSURL(fileURLWithPath: "/Applications/"+tlApp, isDirectory: true) as URL
+                self.appToOpen = tlApp
                 self.corner = true
             }
             // top-right corner
             else if (NSEvent.mouseLocation.x).rounded() >= NSScreen.main!.frame.maxX && (NSEvent.mouseLocation.y).rounded() >= NSScreen.main!.frame.maxY {
-                self.appPath = NSURL(fileURLWithPath: "/Applications/"+trApp, isDirectory: true) as URL
+                self.appToOpen = trApp
                 self.corner = true
             }
 
             // Open application if pointer is at corner
-            if (self.corner) {
+            if (self.corner && self.appToOpen != "") {
+                self.appPath = NSURL(fileURLWithPath: self.appToOpen, isDirectory: true) as URL
                 let path = "/bin"
                 let configuration = NSWorkspace.OpenConfiguration()
                 configuration.arguments = [path]
@@ -59,6 +60,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                                                    configuration: configuration,
                                                    completionHandler: nil)
                 self.corner = false
+                self.appToOpen = ""
             }
         }
 
